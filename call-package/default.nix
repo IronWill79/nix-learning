5baded1141ce873d8849c369f5f08af46077f993
IronWill79 <1820392+IronWill79@users.noreply.github.com>
2024-10-18 11:34:34 +1000

    Refactor `default.nix` to use custom `callPackage` function

--- call-package/default.nix
+++ call-package/default.nix
@@ -1,8 +1,10 @@
 let
   nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/archive/dc2e0028d274394f73653c7c90cc63edbb696be1.tar.gz";
   pkgs = import nixpkgs { config = {}; overlays = []; };
+  callPackage = pkgs.lib.callPackageWith (pkgs // packages);
+  packages = rec {
+    hello = callPackage ./hello.nix { audience = "people"; };
+    hello-folks = hello.override { audience = "folks"; };
+  };
 in
-rec {
-  hello = pkgs.callPackage ./hello.nix { audience = "people"; };
-  hello-folks = hello.override { audience = "folks"; };
-}
+packages
