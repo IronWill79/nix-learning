2ac5e4dd5c8721c824167ec224398a539f3127d8
IronWill79 <1820392+IronWill79@users.noreply.github.com>
2024-10-18 11:18:39 +1000

    Add `default.nix` environment and `hello.nix` build shell script

---
+++ call-package/default.nix
@@ -0,0 +1,5 @@
+let
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/archive/dc2e0028d274394f73653c7c90cc63edbb696be1.tar.gz";
+  pkgs = import nixpkgs { config = {}; overlays = []; };
+in
+pkgs.callPackage ./hello.nix { }
