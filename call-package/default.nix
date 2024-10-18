77ac3d8b14b3eb40b32a3e6acac10a096d3dba9f
IronWill79 <1820392+IronWill79@users.noreply.github.com>
2024-10-18 11:20:59 +1000

    Update to parameterised builds

--- call-package/default.nix
+++ call-package/default.nix
@@ -2,4 +2,6 @@ let
   nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/archive/dc2e0028d274394f73653c7c90cc63edbb696be1.tar.gz";
   pkgs = import nixpkgs { config = {}; overlays = []; };
 in
-pkgs.callPackage ./hello.nix { }
+{
+  hello = pkgs.callPackage ./hello.nix { };
+}
