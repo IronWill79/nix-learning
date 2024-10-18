f8fc753b4d51d0a8a9275a8ab40b01b9045f2f07
IronWill79 <1820392+IronWill79@users.noreply.github.com>
2024-10-18 10:34:33 +1000

    Add a commit-pinned `nixpkgs`

--- shell.nix
+++ shell.nix
@@ -1,5 +1,7 @@
 let
-  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-24.05";
+  # nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-24.05";
+  # dc2e0028d274394f73653c7c90cc63edbb696be1 - nixos-24.05 2024-10-16
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/archive/dc2e0028d274394f73653c7c90cc63edbb696be1.tar.gz";
   pkgs = import nixpkgs { config = {}; overlays = []; };
 in
 
