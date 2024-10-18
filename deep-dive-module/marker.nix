b20157f881c7d0f146f3c0de210402767a1fab0e
IronWill79 <1820392+IronWill79@users.noreply.github.com>
2024-10-18 16:47:55 +1000

    Add size and color to the markers

--- deep-dive-module/marker.nix
+++ deep-dive-module/marker.nix
@@ -1,27 +1,57 @@
 { pkgs, lib, config, ... }:
 let
+  # Returns the uppercased first letter
+  # or a number of a string
+  firstUpperAlnum = str:
+    lib.mapNullable lib.head
+    (builtins.match "[^A-Z0-9]*([A-Z0-9]).*"
+    (lib.toUpper str));
+
+  # Either a color name or `0xRRGGBB`
+  colorType = lib.types.either
+    (lib.types.strMatching "0x[0-9A-Fa-f]{6}")
+    (lib.types.enum [
+      "black" "brown" "green" "purple" "yellow"
+      "blue" "gray" "orange" "red" "white" ]);
+  
   markerType = lib.types.submodule {
     options = {
       location = lib.mkOption {
         type = lib.types.nullOr lib.types.str;
         default = null;
       };
+
       style.label = lib.mkOption {
         type = lib.types.nullOr
           (lib.types.strMatching "[A-Z0-9]");
         default = null;
       };
+
+      style.color = lib.mkOption {
+        type = colorType;
+        default = "red";
+      };
+
+      style.size = lib.mkOption {
+        type = lib.types.enum
+          [ "tiny" "small" "medium" "large" ];
+        default = "medium";
+      };
     };
   };
 
-  userType = lib.types.submodule {
+  userType = lib.types.submodule ({ name, ... }: {
     options = {
       departure = lib.mkOption {
         type = markerType;
         default = {};
       };
     };
-  };
+    config = {
+      departure.style.label = lib.mkDefault
+        (firstUpperAlnum name);
+    };
+  });
 
 in {
 
@@ -53,10 +83,22 @@ in {
     requestParams = let
       paramForMarker = marker:
         let
+          size = {
+            tiny = "tiny";
+            small = "small";
+            medium = "mid";
+            large = null;
+          }.${marker.style.size};
+          
           attributes =
-            lib.optional (marker.style.label != null)
-            "label:${marker.style.label}"
+            lib.optional
+              (marker.style.label != null)
+              "label:${marker.style.label}"
+            ++ lib.optional
+              (size != null)
+              "size:${size}"
             ++ [
+              "color:${marker.style.color}"
               "$(${config.scripts.geocode}/bin/geocode ${
                 lib.escapeShellArg marker.location
               })"
