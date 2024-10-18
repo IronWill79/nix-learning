46f7b50761f6d8fd73409a0eba8fdfdea2053278
IronWill79 <1820392+IronWill79@users.noreply.github.com>
2024-10-18 15:00:58 +1000

    Add parameter to pass to the script

--- deep-dive-module/default.nix
+++ deep-dive-module/default.nix
@@ -8,6 +8,13 @@
     requestParams = lib.mkOption {
       type = lib.types.listOf lib.types.str;
     };
+
+    map = {
+      zoom = lib.mkOption {
+        type = lib.types.nullOr lib.types.int;
+        default = null;
+      };
+    };    
   };
 
   config = {
@@ -23,6 +30,9 @@
     requestParams = [
       "size=640x640"
       "scale=2"
+      (lib.mkIf (config.map.zoom != null)
+        "zoom=${toString config.map.zoom}")
     ];
+
   };
 }
