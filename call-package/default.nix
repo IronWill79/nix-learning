6e7f6a120da1bb09ece8dec4ff6764ecb0b181f9
IronWill79 <1820392+IronWill79@users.noreply.github.com>
2024-10-18 11:30:48 +1000

    Add override config in `default.nix`

--- call-package/default.nix
+++ call-package/default.nix
@@ -2,6 +2,7 @@ let
   nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/archive/dc2e0028d274394f73653c7c90cc63edbb696be1.tar.gz";
   pkgs = import nixpkgs { config = {}; overlays = []; };
 in
-{
+rec {
   hello = pkgs.callPackage ./hello.nix { audience = "people"; };
+  hello-folks = hello.override { audience = "folks"; };
 }
