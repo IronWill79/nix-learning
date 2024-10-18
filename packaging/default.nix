695c09ffc344865f0c5c3f64baa98b31a6705853
IronWill79 <1820392+IronWill79@users.noreply.github.com>
2024-10-18 10:40:00 +1000

    Add `default.nix` to set up nixpkgs

---
+++ packaging/default.nix
@@ -0,0 +1,7 @@
+let
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/archive/dc2e0028d274394f73653c7c90cc63edbb696be1.tar.gz";
+  pkgs = import nixpkgs { config = {}; overlays = []; };
+in
+{
+  hello = pkgs.callPackage ./hello.nix { };
+}
