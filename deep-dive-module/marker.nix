965642611ea31a60bba0e1d8fe8cb1f157f04537
IronWill79 <1820392+IronWill79@users.noreply.github.com>
2024-10-18 16:18:55 +1000

    Setup markers to take locations from `config.users.<name>.departure.location`

---
+++ deep-dive-module/marker.nix
@@ -0,0 +1,54 @@
+{ pkgs, lib, config, ... }:
+let
+  markerType = lib.types.submodule {
+    options = {
+      location = lib.mkOption {
+        type = lib.types.nullOr lib.types.str;
+        default = null;
+      };
+    };
+  };
+
+  userType = lib.types.submodule {
+    options = {
+      departure = lib.mkOption {
+        type = markerType;
+        default = {};
+      };
+    };
+  };
+
+in {
+
+  options = {
+    users = lib.mkOption {
+      type = lib.types.attrsOf userType;
+    };
+    
+    map.markers = lib.mkOption {
+      type = lib.types.listOf markerType;
+    };
+  };
+
+  config = {
+    map.markers = lib.filter
+      (marker: marker.location != null)
+      (lib.concatMap (user: [
+        user.departure
+      ]) (lib.attrValues config.users));
+
+    map.center = lib.mkIf
+      (lib.length config.map.markers >= 1)
+      null;
+
+    map.zoom = lib.mkIf
+      (lib.length config.map.markers >= 2)
+      null;
+
+    requestParams = let
+      paramForMarker =
+        builtins.map (marker: "$(${config.scripts.geocode}/bin/geocode ${
+          lib.escapeShellArg marker.location})") config.map.markers;
+    in [ "markers=\"${lib.concatStringsSep "|" paramForMarker}\"" ];
+  };
+}
