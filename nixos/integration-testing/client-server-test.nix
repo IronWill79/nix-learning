d9335a266a9cf56af8e2254998543b7474a3362e
IronWill79 <1820392+IronWill79@users.noreply.github.com>
2024-10-18 21:13:47 +1000

    Add `client-server-test.nix` integration test between server and client

---
+++ nixos/integration-testing/client-server-test.nix
@@ -0,0 +1,32 @@
+let
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/archive/dc2e0028d274394f73653c7c90cc63edbb696be1.tar.gz";
+  pkgs = import nixpkgs { config = {}; overlays = []; };
+in
+
+pkgs.testers.runNixOSTest {
+  name = "client-server-test";
+
+  nodes.server = {
+    networking = {
+      firewall = {
+        allowedTCPPorts = [ 80 ];
+      };
+    };
+    services.nginx = {
+      enable = true;
+      virtualHosts."server" = {};
+    };
+  };
+
+  nodes.client = { pkgs, ... }: {
+    environment.systemPackages = with pkgs; [
+      curl
+    ];
+  };
+
+  testScript = ''
+    server.wait_for_unit("default.target")
+    client.wait_for_unit("default.target")
+    client.succeed("curl http://server/ | grep -o \"Welcome to nginx!\"")
+  '';
+}
